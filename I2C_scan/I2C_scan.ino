f1503f3346986d6be2bfa081ca250045612b7b6a
Thakdanai Sirisombat <48859187+thakdanaiza@users.noreply.github.com>
2023-08-26 11:18:24 +0700

    Add files via upload

---
+++ I2C_scan/I2C_scan.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+
+#define I2C_SDA_2            21
+#define I2C_SCL_2            22
+TwoWire I2CBME = TwoWire(1);
+void setup()
+{
+  I2CBME.begin(I2C_SDA_2, I2C_SCL_2, 400000);
+ 
+  Serial.begin(9600);
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("\nI2C Scanner");
+}
+ 
+ 
+void loop()
+{
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    I2CBME.beginTransmission(address);
+    error = I2CBME.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+ 
+  delay(1000);           // wait 5 seconds for next scan
+}
